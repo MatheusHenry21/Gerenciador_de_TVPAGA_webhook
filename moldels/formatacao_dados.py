5ca0f8fa0ec202fb9ca4ce0c4e17ece998f5f6e4
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-02 11:27:18 -0300

    Modelos de objetos

--- moldels/formatacao_dados.py
+++ moldels/formatacao_dados.py
@@ -2,16 +2,17 @@
 from data.testeUnique.cadastroCelular import check_unique_celular
 from maneger.exceptions.exceptions_routers import (
     NumeroExistenteError,
-    NomeVazio
+    QuantidadeNumero,
+    CelularInvalido
     )
 
 def nome_formatacao(nome):
-    if not nome or nome == '' or nome == None:
-        raise NomeVazio("Nome não pode está vazio")
     nome = nome.title()
     return nome
 
 def formatacao_celular(celular):
+    if not celular.isdigit():
+        raise CelularInvalido("Digite apenas números.")
     if len(celular) == 10:
         celular10 = (f"({celular[:2]})9{celular[2:6]}-{celular[6:]}")
         return celular10
@@ -19,6 +20,8 @@ def formatacao_celular(celular):
     elif len(celular) == 11:
         celular11 = (f"({celular[:2]}){celular[2:7]}-{celular[7:]}")
         return celular11
+    else:
+        raise QuantidadeNumero("")
 
 def celular_novo_contato(numCelular):
     celularTeste = formatacao_celular(str(numCelular))
